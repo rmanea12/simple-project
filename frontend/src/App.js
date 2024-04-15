683bc477be1c970106fb4e45b09f1f89e7de91a7
rmanea12 <rares.manea@oup.com>
2024-04-15 11:57:25 +0300

    frontend commit

---
+++ frontend/src/App.js
@@ -0,0 +1,53 @@
+import React, { useState, useEffect } from "react";
+
+import Header from "./components/Header/Header";
+import NewProduct from "./components/Products/NewProduct";
+import ProductList from "./components/Products/ProductList";
+import "./App.css";
+
+function App() {
+  const [loadedProducts, setLoadedProducts] = useState([]);
+
+  //EXPLAIN WHAT IS USEFFECT
+  useEffect(() => {
+    //CREATE A RESPONSE CONSTANT AND FETCH DATA, ALSO JSON IT AND ADD IT TO A RESPONSEDATA CONSTANT AND SET THE LOADED PRODUCTS
+    //WITH THE RESPONSEDATA
+    const fetchProducts = async () => {};
+
+    fetchProducts();
+  }, []);
+
+  const addProductHandler = async (productName, productPrice) => {
+    try {
+      const newProduct = {
+        title: productName,
+        price: +productPrice,
+      };
+
+      //FETCH THE PRODUCT ROUTE TO CREATE A PRODUCT WITH A POST METHOD
+
+      const responseData = await response.json();
+
+      setLoadedProducts((prevProducts) => {
+        return prevProducts.concat({
+          ...newProduct,
+          id: responseData.product.id,
+        });
+      });
+    } catch (error) {
+      alert(error.message || "Something went wrong!");
+    }
+  };
+
+  return (
+    <React.Fragment>
+      <Header />
+      <main>
+        <NewProduct onAddProduct={addProductHandler} />
+        //YOU NEED TO ADD A COMPONENT HERE, WHAT IS IT?
+      </main>
+    </React.Fragment>
+  );
+}
+
+export default App;
