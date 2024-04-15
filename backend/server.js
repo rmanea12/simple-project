54fd6a3dc70cc7fa085a5569e0ec3b481423df2d
rmanea12 <rares.manea@oup.com>
2024-04-15 11:46:36 +0300

    initial commit

---
+++ backend/server.js
@@ -0,0 +1,29 @@
+const express = require("express");
+const bodyParser = require("body-parser");
+const { uuid } = require("uuidv4");
+
+const app = express();
+
+const DUMMY_PRODUCTS = []; // not a database, just some in-memory storage for now
+
+app.use(bodyParser.json());
+
+// CORS Headers => Required for cross-origin/ cross-server communication
+app.use((req, res, next) => {
+  res.setHeader("Access-Control-Allow-Origin", "*");
+  res.setHeader(
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested-With, Content-Type, Accept, Authorization"
+  );
+  res.setHeader(
+    "Access-Control-Allow-Methods",
+    "GET, POST, PATCH, DELETE, OPTIONS"
+  );
+  next();
+});
+
+//CREATE A GET ROUTE TO RETRIEVE ALL THE DUMMY_PRODUCTS
+
+//CREATE A POST ROUTE TO CREATE A PRODUCT
+
+app.listen(5000); // start Node + Express server on port 5000
